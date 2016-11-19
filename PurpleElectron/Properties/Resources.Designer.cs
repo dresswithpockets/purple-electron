0cb518fe8201660dfa77504d59b0fef67a0f9aec
Ashley Horton <dresswithpockets@pm.me>
2016-11-19 05:07:30 -0700

    Automated resources changes

--- PurpleElectron/Properties/Resources.Designer.cs
+++ PurpleElectron/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace PurpleElectron.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
